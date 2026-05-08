446bfd5b3eb1bc89c2eb8c043e6c300e5f3db0c1
Icey-0528 <92418802+Icey-0528@users.noreply.github.com>
2026-05-07 21:11:09 -0700

    fixedB only, momentum only, fixedB+momentum

--- zoo/fedncf_lora_momentum_only_2.py
+++ zoo/fedncf_lora_momentum_only_2.py
@@ -433,7 +433,7 @@ class Server(ServerBase):
         return val_logs
 
 
-class FedNCF_Lora:
+class FedNCF_Lora_Momentum_Only:
     def __init__(self, 
                  dataload: BaseDataLoaderFL,
                  clients_num_per_turn, 
