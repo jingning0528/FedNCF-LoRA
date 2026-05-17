0eeb2c3c68edba52298cffe71e23a4713911309e
Icey-0528 <92418802+Icey-0528@users.noreply.github.com>
2026-05-16 21:27:29 -0700

    lora analyze

--- zoo/__init__.py
+++ zoo/__init__.py
@@ -13,4 +13,5 @@ from .fedncf_lora_momentumA_fixedB import FedNCF_Lora_MomentumA_FixedB
 from .fedncf_lora_momentumB_fixedA import FedNCF_Lora_MomentumB_FixedA
 
 from .fedncf_lora_momentumA_fixedB_finetune import FedNCF_Lora_MomentumA_FixedB_Finetune
+from .fedncf_lora_momentumA_fixedB_finetune_analyze import FedNCF_Lora_MomentumA_FixedB_Finetune_Analyze
 from .fedncf_lora_analyze import FedNCF_Lora_Analyze
